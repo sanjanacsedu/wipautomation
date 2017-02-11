04aa2261c3eea9cf28b3f93ee996cd617eafedeb
sanjanacsedu <sanjanacsedu@gmail.com>
2017-02-10 21:37:24 -0800

    two past date using java date api

---
+++ src/com/wip/automation/exercise4/PastDate.java
@@ -0,0 +1,16 @@
+package com.wip.automation.exercise4;
+
+import java.time.LocalDate;
+
+public class PastDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate dateOfBirth1 = LocalDate.of(2010, 01, 14);
+		System.out.println("Your Date of birth is : " + dateOfBirth1);
+
+		LocalDate dateOfBirth2 = LocalDate.of(2010, 02, 2);
+		System.out.println("Your Date of birth is : " + dateOfBirth2);
+	}
+
+}
