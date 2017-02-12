4a739e725b42d71c8b2cbb7371305e5e684dc11c
sanjanacsedu <sanjanacsedu@gmail.com>
2017-02-11 21:03:34 -0800

    array operations using functions

--- src/com/wip/automation/exercise4/ArrayOperations.java
+++ src/com/wip/automation/exercise4/ArrayOperations.java
@@ -4,54 +4,86 @@ import java.time.LocalDate;
 
 public class ArrayOperations {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] sumArray = new int[] { 2, 1, 5, 6 };
-		int sum = 0;
-		for (int i = 0; i < sumArray.length; i++)
-			sum += sumArray[i];
-		System.out.println("Summation is: " + sum);
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    int[] originalArray = new int[] {2, 1, 5, 6};
+    
+    int result = sumArray(originalArray);
+    System.out.println("sum is " + result);
+    
+    // Find minimum array elements
+    int minimum = minimum(originalArray);
+    System.out.println("Minimum element: " + minimum);
 
-		// Find minimum array elements
+    // Find max array elements
+    int max = maximum(originalArray);
+    System.out.println("Maximum elemet: " + max);
 
-		int minimum = sumArray[0];
-		for (int i = 1; i < sumArray.length; i++)
-			if (minimum > sumArray[i])
-				minimum = sumArray[i];
-		System.out.println("Minimum element: " + minimum);
 
-		// Find max array elements
 
-		int max = sumArray[0];
-		for (int i = 1; i < sumArray.length; i++)
-			if (max < sumArray[i])
-				max = sumArray[i];
+    int[] copiedArray = copytoArray(originalArray);
+    System.out.println("copied array: ");
+    for (int i = 0; i < copiedArray.length; i++) {
+      System.out.println(copiedArray[i]);
+    }
 
-		System.out.println("Maximum elemet: " + max);
+    int[] copyArrayEnd = copytoArrayEnd(originalArray);
+    System.out.println("reverse array: ");
+    for (int i = 0; i < copyArrayEnd.length; i++) {
+      System.out.println(copyArrayEnd[i]);
+    }
 
-		int[] originalArray = { 8, 12, 6, 5, 19 };
-		int[] copyArray;
-		int[] copyArrayEnd;
-		copyArray = new int[originalArray.length];
-		copyArrayEnd = new int[originalArray.length];
+  }
 
-		for (int i = 0; i < originalArray.length; i++) {
-			copyArray[i] = originalArray[i];
-		}
+  static int minimum(int[] originalArray) {
+    // System.out.println("Hello world");
+    int minimum = originalArray[0];
+    for (int i = 1; i < originalArray.length; i++)
+      if (minimum > originalArray[i])
+        minimum = originalArray[i];
+    return minimum;
+  }
 
-		for (int i = originalArray.length - 1, j = 0; i >= 0; i--, j++) {
-			copyArrayEnd[j] = originalArray[i];
-		}
 
-		System.out.println("copied array: ");
-		for (int i = 0; i < copyArray.length; i++) {
-			System.out.println(copyArray[i]);
-		}
-		System.out.println("reverse array: ");
-		for (int i = 0; i < copyArrayEnd.length; i++) {
-			System.out.println(copyArrayEnd[i]);
-		}
+  static int maximum(int[] originalArray) {
+    // System.out.println("Hello world");
+    int max = originalArray[0];
+    for (int i = 1; i < originalArray.length; i++)
+      if (max < originalArray[i])
+        max = originalArray[i];
+    return max;
+  }
 
-	}
+
+  static int sumArray(int[] originalArray) {
+    // System.out.println("Hello world");
+    int sum = 0;
+    for (int i = 0; i < originalArray.length; i++)
+      sum += originalArray[i];
+    return sum;
+  }
+
+
+
+  static int[] copytoArray(int[] originalArray) {
+    // System.out.println("Hello world");
+    int[] copyArray = new int[originalArray.length];
+    for (int i = 0; i < originalArray.length; i++) {
+      copyArray[i] = originalArray[i];
+    }
+    return copyArray;
+  }
+
+
+  static int[] copytoArrayEnd(int[] originalArray) {
+    // System.out.println("Hello world");
+    int[] copyArrayEnd = new int[originalArray.length];
+    for (int i = originalArray.length - 1, j = 0; i >= 0; i--, j++) {
+      copyArrayEnd[j] = originalArray[i];
+    }
+    return copyArrayEnd;
+  }
+
+
 
 }
