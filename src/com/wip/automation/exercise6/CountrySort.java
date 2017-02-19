9607f768011d561e35e71a87f2706c05e986592b
sanjanacsedu <sanjanacsedu@gmail.com>
2017-02-18 23:16:45 -0800

    country sort

--- src/com/wip/automation/exercise6/CountrySort.java
+++ src/com/wip/automation/exercise6/CountrySort.java
@@ -6,16 +6,16 @@ public class CountrySort {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    
-    String[] countries = new String[]{"Bangladesh","USA","India", "Pakistan"};
-    //sort String array using sort method
+
+    String[] countries = new String[] {"Bangladesh", "USA", "India", "Pakistan"};
+    // sort String array using sort method
     Arrays.sort(countries);
-   
+
     System.out.println("Sorted country names: ");
-   
-    //print sorted country
-    for(int i=0; i < countries.length; i++){
-            System.out.println(countries[i]);
+
+    // print sorted country
+    for (int i = 0; i < countries.length; i++) {
+      System.out.println(countries[i]);
     }
 
   }
